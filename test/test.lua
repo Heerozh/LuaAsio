7878fe38e8985d58c4af98b14d317ccbb9dfbb75
Heerozh <heeroz@gmail.com>
2018-04-03 18:02:20 +0800

    test complete

--- test/test.lua
+++ test/test.lua
@@ -80,6 +80,7 @@ end
 --------------------------------------------------
 
 --test network
+for i=1,1000
 do io.write('---- C Asio Test ----')
 
     -- non-ip address should return nil
@@ -96,40 +97,75 @@ do io.write('---- C Asio Test ----')
 
     -- server
     function connection_th(con)
-        print('appected.')
+        --print('server')
         local data, err = con:read(5)  
-        print('server readed:', data, err)        
+        --print('server', data, 'readed', e)
+        assert(data == 'ping1' or data == 'ping2' or data == 'ping3')      
         con:write(data .. '-pong')  
         con:close()
-        print('server closed', data)
     end
     
     local s = asio.server('127.0.0.1', 31234, function(con) 
         asio.spawn_light_thread(connection_th, con) 
     end)
-
+    local total =0
     local ping_send = function(text) 
+        --print('client', text)
         local con, e = asio.connect('localhost', '31234')
+        assert(con, e)
+        --print('client', text, 'conned', e)
         local ok, e = con:write(text)
-        print('client write ok:', ok, e)
         local data, err = con:read(10)
-        print('client readed:', data, err)                
+        assert(data == 'ping1-pong' or data == 'ping2-pong' or data == 'ping3-pong')         
         con:close()
-        --asio.stop()
-        print('client closed', text)
-        if text =='stop_' then
+        total = total + 1
+        if total ==3 then
             asio.destory_server(s)
             s = nil
         end
     end
-    
     asio.spawn_light_thread(ping_send, 'ping1')
     asio.spawn_light_thread(ping_send, 'ping2')
-    asio.spawn_light_thread(ping_send, 'stop_')
-    print('run')
+    asio.spawn_light_thread(ping_send, 'ping3')
     asio.run() 
 
 end io.write(' \t\t[OK]\n')
 
+--bench
+do io.write('---- C Asio Bench ----')
+
+    --benchmark
+    local connects = 10
+    local sends = 1000
+    local function client_bench()
+        local con, e = asio.connect('localhost', '31234')
+        for i=1,sends do
+            con:write('123456789|123456789|123456789|123456789|123456789|')
+            con:read(50)
+        end
+        con:close()
+    end
+    local total = 0
+    local s
+    local function server_bench(con)
+        for i=1,sends do
+            con:read(50)
+            con:write('123456789|123456789|123456789|123456789|123456789|')
+            total = total + 1
+        end
+        con:close()      
+        if total == sends*connects then
+            asio.destory_server(s)
+        end
+    end
+    s = asio.server('127.0.0.1', 31234, function(con) 
+        asio.spawn_light_thread(server_bench, con) 
+    end)
+    for i = 1,connects do
+        asio.spawn_light_thread(client_bench)
+    end
+    asio.run() 
+
+end io.write(' \t\t[OK]\n')
 
 print('All Tests passed.')
